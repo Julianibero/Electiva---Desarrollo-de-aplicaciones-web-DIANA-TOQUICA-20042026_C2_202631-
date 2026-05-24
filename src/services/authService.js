2a03d0b56df7a7389e1ffacd6aa9fe2c7cd7f81a
Julianibero <jvegajoy@estudiante.ibero.edu.co>
2026-05-24 14:39:11 -0500

    Delete src directory

--- src/services/authService.js
+++
@@ -1,5 +0,0 @@
-// src/services/authService.js
-import api from '../api/axiosInstance';
-
-export const loginRequest  = (credentials) => api.post('/auth/login', credentials);
-export const logoutRequest = ()             => api.post('/auth/logout');
